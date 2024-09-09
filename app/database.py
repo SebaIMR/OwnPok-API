a20979afa867304e60e90a09722a441ae3d33ea9
Camila <camila.escobedo.f@mail.pucv.cl>
2024-09-08 22:30:40 -0300

    comentario de prueba

--- app/database.py
+++ app/database.py
@@ -5,6 +5,7 @@ from .config import settings
 from google.cloud.sql.connector import Connector
 
 # Configurar el conector de Google Cloud SQL
+## PROBANDING ALGO
 connector = Connector()
 
 def getconn():
